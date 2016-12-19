036b6ec76320b8d4b82a243f6232dd898d7c2a4b
brandonxiang <1542453460@qq.com>
2016-12-19 11:02:44 +0800

    add unittest should.js

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 var intepolator = require("./index")
 var assert = require('assert')
+var should = require('should')
 
-describe('One point is between two points', function() {
+describe('One point', function() {
     describe('#interpolator()', function() {
-        it('should between y values of two points', function() {
+        it('should between y values of two points when it is between two points', function() {
             var result = intepolator([{ x: 3, y: 14 }, { x: 8, y: 22 }], 7, 1.5)
-            assert.equal(Math.floor(result), 20)
+            var approximate = Math.floor(result)
+            approximate.should.be.exactly(20)
+        })
+
+        it('should equal edge value when it is out two points',function(){
+          var result = intepolator([{ x: 3, y: 14 }, { x: 8, y: 22 }], 9, 1.5)
+          result.should.be.exactly(22)
         })
     })
 })
