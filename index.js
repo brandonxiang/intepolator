bd899e8c5174a1ca50cebebd6e2e74372a4fd14f
brandonxiang <1542453460@qq.com>
2016-12-19 10:37:20 +0800

    add unitest

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 module.exports = function(data, value, n) {
-    if(!data.length){
+    if (!data.length) {
         return
     }
-    for(var i=0; i<data.length;i++){
-        // if(!data[i+1]){
-        //     if(value >= data[i].x){
-        //         return data[i].y
-        //     }else{
-        //         console.log("1")
-        //         return data[i].y
-        //     }
-        // }
-
-        // if(data[i].x<value&&value<data[i+1].x){
-        //     return interpolateByTwoPts(data[i].x,data[i+1].x,data[i].y,data[i+1].y,value,n)
-        if(value < data[0].x){
-                return data[i].y
-        }else if(value > data[i].x){
-            if(data[i+1]){
-            if(value < data[i+1].x){
-    return interpolateByTwoPts(data[i].x,data[i+1].x,data[i].y,data[i+1].y,value,n)
-
-            }
-            }else{
+    for (var i = 0; i < data.length; i++) {
+        if (value < data[0].x) {
+            return data[i].y
+        } else if (value > data[i].x) {
+            if (data[i + 1]) {
+                if (value < data[i + 1].x) {
+                    return interpolateByTwoPts(data[i].x, data[i + 1].x, data[i].y, data[i + 1].y, value, n)
+                }
+            } else {
                 return data[i].y
             }
 
@@ -30,11 +18,10 @@ module.exports = function(data, value, n) {
     }
 }
 
-function interpolateByTwoPts(x1,x2,y1,y2,x3,n){
+function interpolateByTwoPts(x1, x2, y1, y2, x3, n) {
     var xn1 = Math.pow(x1, n);
     var xn2 = Math.pow(x2, n);
     var a = (y2 - y1) / (xn2 - xn1);
     var xn3 = Math.pow(x3, n);
     return y1 + a * (xn3 - xn1)
 }
-
--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 var intepolator = require("./index")
+var assert = require('assert')
 
-console.log(intepolator([{x:3, y:14},{x: 8,y: 22}], 7, 1.5))
+describe('One point is between two points', function() {
+    describe('#interpolator()', function() {
+        it('should between y values of two points', function() {
+            var result = intepolator([{ x: 3, y: 14 }, { x: 8, y: 22 }], 7, 1.5)
+            assert.equal(Math.floor(result), 20)
+        })
+    })
+})
